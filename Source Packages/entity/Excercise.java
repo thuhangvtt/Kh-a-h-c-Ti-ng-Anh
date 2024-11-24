44a67854060346c1b9ee95e9d70cafa811962148
thuhangvtt <vuthithuhang24cv@gmail.com>
2024-11-24 19:21:01 +0700

    Create Excercise.java

---
+++ Source Packages/entity/Excercise.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Exercise {
+    private int exerciseId;
+    private String exerciseName;
+    private String exerciseLink;
+
+    public int getExerciseId() {
+        return exerciseId;
+    }
+
+    public void setExerciseId(int exerciseId) {
+        this.exerciseId = exerciseId;
+    }
+
+    public String getExerciseName() {
+        return exerciseName;
+    }
+
+    public void setExerciseName(String exerciseName) {
+        this.exerciseName = exerciseName;
+    }
+
+    public String getExerciseLink() {
+        return exerciseLink;
+    }
+
+    public void setExerciseLink(String exerciseLink) {
+        this.exerciseLink = exerciseLink;
+    }
+}
