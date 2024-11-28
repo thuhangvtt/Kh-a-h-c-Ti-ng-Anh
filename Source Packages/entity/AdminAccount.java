1292a732eb0dedf2df7e6c2013dc728822327848
Na22-1814 <0982duong@gmail.com>
2024-11-28 17:32:28 +0700

    Create AdminAccount.java

---
+++ Source Packages/entity/AdminAccount.java
@@ -0,0 +1,70 @@
+package entity;
+public class AdminAccount
+{
+
+    private int id;
+    private String username;
+    private String password;
+    private String name;
+    private String phone;
+    private String email;
+    
+    public AdminAccount() {} 
+    
+    public AdminAccount(int id, String username, String password, String name, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
